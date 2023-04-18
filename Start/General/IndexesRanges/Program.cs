4420047721257ab3c5ad166091b9993a59b97ac5
fcafici <fcafici@hexacta.com>
2023-04-18 13:02:22 -0300

    Index and Ranges

--- Start/General/IndexesRanges/Program.cs
+++ Start/General/IndexesRanges/Program.cs
@@ -9,12 +9,31 @@ string[] words = new string[]
 };
 
 // The index operator provides access to array elements
-Console.WriteLine(words[1]);
+//Console.WriteLine(words[1]);
 
 // TODO: The index-from-end operator indexes from the end of a sequence
 // (Note that the end is not zero-based, ^1 is the last item)
+//Console.WriteLine(words[words.Length - 1]);
+//Console.WriteLine(words[^1]);
 
 // TODO: The range operator (..) defines a range
+string[] wordRange = words[2..5];
+//Console.WriteLine($"{string.Join(",",wordRange)}");
 
+//wordRange = words[..];
+//Console.WriteLine($"{string.Join(",",wordRange)}");
+
+
+//wordRange = words[2..];
+//Console.WriteLine($"{string.Join(",",wordRange)}");
+
+//wordRange = words[..5];
+//Console.WriteLine($"{string.Join(",",wordRange)}");
 
 // TODO: Indexes and ranges can be variables too
+Index idx = ^4;
+Console.WriteLine(words[idx]);
+
+Range rng = 3..^1;
+wordRange = words[rng];
+Console.WriteLine($"{string.Join(",",wordRange)}");
