1024ba8b690e6fe338bc7f21d52d2cbe01459ada
fcafici <fcafici@hexacta.com>
2023-04-18 13:06:01 -0300

    Numbers and literals

--- Start/General/NumberLiterals/Program.cs
+++ Start/General/NumberLiterals/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Example file for LinkedIn Learning Course Advanced C#: Language Features by Joe Marini
 
-// TODO: Starting in C# 7, you can use _ as a number separator
-int d = 123456;
-float f = 1234.5f;
-var x = 0xABCDEF;
-var b = 0b1101111010010010;
+// TODO: Starting in C# 7, you can use _ as a number separator. NO AFECTA AL RESULTADO
+int d = 123_456; 
+float f = 1_234.5f;
+var x = 0xAB_CD_EF;               // hexa
+var b = 0b1101_1110_1001_0010;     // binario
 
 Console.WriteLine($"{d}");
 Console.WriteLine($"{f}");
