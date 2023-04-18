5b948c2ae24b18ef0b141e05d9ea02d501ebbbe3
fcafici <fcafici@hexacta.com>
2023-04-18 14:32:36 -0300

    Deconstruct Custom

--- Start/General/DeconstructCustom/Planet.cs
+++ Start/General/DeconstructCustom/Planet.cs
@@ -16,11 +16,26 @@ public class Planet
     }
 
     // TODO: Define a Deconstruct method to return the name and moon count
-
+    public void Deconstruct(out string name, out int moons) 
+    {
+        name = Name;
+        moons = MoonCount;
+    }
 
     // TODO: Define a Deconstruct method to return the name, moon count, and radius
-
+    public void Deconstruct(out string name, out int moons, out int radius) 
+    {
+        name = Name;
+        moons = MoonCount;
+        radius = Radius;
+    }
 
     // TODO: Defining a Deconstruct with the name number of parameters will cause an error
-
+    /*
+    public void Deconstruct(out string name, out int distance)
+    {
+        name = Name;
+        distance = DistanceFromSunKm;
+    }
+    */
 }
